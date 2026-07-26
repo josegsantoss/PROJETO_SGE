2e3c60711e9537196356b16748b6442d48e81f84
joeltonj <joeltonj.73@gmail.com>
2026-07-26 15:15:25 -0300

    Refactor imports and update __all__ exports

--- back-end/CRUD's/__init__.py
+++ back-end/CRUD's/__init__.py
@@ -1,50 +1,12 @@
-from database.CRUD_CU import (
-    verificarLogin,
-    cadastrarLogin,
-    listarLogin
-)
-from database.CRUD_Categorias import (
-    listar_categorias,
-    criar_categoria
-)
-from database.CRUD_Produtos import (
-    listar_produtos,
-    listar_produtos_por_categoria,
-    buscar_produto,
-    criar_produto,
-    atualizar_produto,
-    atualizar_estoque,
-    deletar_produto
-)
-from database.CRUD_CLIENTES import (
-    listar_clientes,
-    buscar_cliente,
-    buscar_clientes_por_cpf,
-    buscar_cliente_por_cnpj,
-    buscar_cliente_por_celular,
-    buscar_clientes_por_nome,
-    buscar_clientes_por_cep,
-    cadastrar_cliente,
-    atualizar_cliente,
-    deletar_cliente
-)
-from database.CRUD_CE import (
-    listar_equipamentos,
-    buscar_equipamento,
-    cadastrar_equipamento,
-    atualizar_equipamento,
-    excluir_equipamento
-)
-from database.CRUD_orcamento import (
-    listar_orcamentos,
-    buscar_orcamento,
-    buscar_orcamento_por_numero,
-    buscar_orcamento_por_cliente,
-    criar_orcamento,
-    atualizar_orcamento,
-    excluir_orcamento,
-    calcular_total_orcamento
-)
+from CRUDS.CADASTRO_USUARIOS import *
+
+from CRUDS.CADASTRO_PRODUTOS import *
+
+from CRUDS.CLIENTES import * 
+
+from CRUDS.CADASTRO_EQUIPAMENTOS import *
+
+from CRUDS.ORCAMENTO import *
 
 __all__ = [
     'verificarLogin',
